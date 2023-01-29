615e4413a9805d466e7ff40b9d9d1c35846c200a
yuto miyata <yuto.miya525ta@gmail.com>
2023-01-29 11:15:30 +0900

    urlの修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./Footer.css";
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footerBlock'>
-      <p>name:Yuto Miyata</p>
+        <Link to='/'><button>Home</button></Link>
+        <Link to='/profile'><button>Profile</button></Link>
+        <Link to='/works'><button>Works</button></Link>
     </div>
   )
 }
--- src/components/WorksContent.js
+++ src/components/WorksContent.js
@@ -8,45 +8,36 @@ const WorksContent = () => {
     theme:'reactredux',
     language:['react,','redux'],
     ingenuity:'頑張った',
-    url:'https://master.d1ifpyx30908pf.amplifyapp.com',
-    // github:'https://github.com/YutoMiyata/to-do-app-using-react',
-    github:'https://google.com',
     }
   ]
   return (
-    <>
+    <div className='worksComponents'>
       {works.map((work) => {
         return (
-          <div className='worksBlock'>
-            <div className='worksPhoto'>
-              <img src={work.imgUrl} className = "workPhotoImg"/>
-            </div>
-            <div className='worksContent worksContentFlex'>
-                <div className='worksContentFlex_title'>
-                  <h3>テーマ</h3>
-                  <h3>使用言語</h3>
-                  <h3>工夫点</h3>
-                  <h3>URL</h3>
-                  <h3>GitHub</h3>
-                </div>
-                <div className='worksContentFlex_content'>
-                  <h3>{work.theme}</h3>
-                  <h3>{work.language}</h3>
-                  <h3>{work.ingenuity}</h3>
-                  <h3>
-                    <button><a href='{work.url}'>こちらから</a></button>
-                  </h3>
-                  <h3>
-                    <button><a href='{work.github}'>GitHub</a></button>
-                  </h3>
-                </div>
-            </div>
-        </div>
+            <div className='worksBlock'>
+              <div className='worksPhoto'>
+                <img src={work.imgUrl} className = "workPhotoImg"/>
+              </div>
+              <div className='worksContent worksContentFlex'>
+                  <div className='worksContentFlex_title'>
+                    <h3>テーマ</h3>
+                    <h3>使用言語</h3>
+                    <h3>工夫点</h3>
+                  </div>
+                  <div className='worksContentFlex_content'>
+                    <h3>{work.theme}</h3>
+                    <h3>{work.language}</h3>
+                    <h3>{work.ingenuity}</h3>
+                  </div>
+              </div>
+            </div>     
         )
       })}
-      <button><a href='https://master.d1ifpyx30908pf.amplifyapp.com'>button</a></button>
+      <div className='urlButton'>
+        <button><a href='https://master.d1ifpyx30908pf.amplifyapp.com'>Url Button</a></button>
+      </div>
 
-    </>
+    </div>
 
   )
 }
