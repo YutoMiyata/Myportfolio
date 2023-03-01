81a6c24bea3dafd7900f31a58c313e8f342bfeba
yuto miyata <yuto.miya525ta@gmail.com>
2023-03-01 17:40:48 +0900

    profile画面の修正

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -21,28 +21,31 @@ const Profile = () => {
             <div className='myprofileImg'>
                 <img src = {myface} />
             </div>
-    
-            <div className = 'myProfileContent'>
-                <h2>自己紹介</h2>
-                <table className='profileTable'>
-                    <tr>
-                        <th className='profileTitle'>名前</th>
-                        <th className='profileContent'>宮田優人</th>
-                    </tr>
-                    <tr>
-                        <td className='profileTitle'>フリガナ</td>
-                        <td className='profileContent'>ミヤタ ユウト</td>
-                    </tr>
-                    <tr>
-                        <td className='profileTitle'>年齢</td>
-                        <td className='profileContent'>22歳</td>
-                    </tr>
-                    <tr>
-                        <td className='profileTitle'>大学名</td>
-                        <td className='profileContent'> 法政大学スポーツ健康学部</td>
-                    </tr>
-                </table>
+            
+            <div className='myProfileContenteBlock'>
+                <div className = 'myProfileContent'>
+                    <h2>自己紹介</h2>
+                    <table className='profileTable'>
+                        <tr>
+                            <th className='profileTitle'>名前</th>
+                            <th className='profileContent'>宮田優人</th>
+                        </tr>
+                        <tr>
+                            <td className='profileTitle'>フリガナ</td>
+                            <td className='profileContent'>ミヤタ ユウト</td>
+                        </tr>
+                        <tr>
+                            <td className='profileTitle'>年齢</td>
+                            <td className='profileContent'>22歳</td>
+                        </tr>
+                        <tr>
+                            <td className='profileTitle'>大学名</td>
+                            <td className='profileContent'> 法政大学スポーツ健康学部</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
+
         </div>
 
         <ProfilePrograming />
