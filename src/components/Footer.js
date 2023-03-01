740ebd0b021428d3db37c4bee94e0ecc0a1e6999
yuto miyata <yuto.miya525ta@gmail.com>
2023-03-01 17:27:40 +0900

    footerのレイアウト修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,19 @@ import "./Footer.css";
 import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
-    <div className='footerBlock'>
-      <div className='footerFlex'>
-        <Link to='/'><p>Home</p></Link>
-        <Link to='/profile'><p>Profile</p></Link>
-        <Link to='/works'><p>Works</p></Link>
+    <footer>
+      <div className='footerBlock'>
+        <div className='footerFlex'>
+          <Link to='/'><p>Home</p></Link>
+          <Link to='/profile'><p>Profile</p></Link>
+          <Link to='/works'><p>Works</p></Link>
+        </div>
+        <div className='GitHubBlock'>
+          <p><a href='https://github.com/YutoMiyata'>GitHub</a></p>
+        </div>
       </div>
-      <div className='GitHubBlock'>
-        <p><a href='https://github.com/YutoMiyata'>GitHub</a></p>
-      </div>
-    </div>
+    </footer>
+
   )
 }
 
